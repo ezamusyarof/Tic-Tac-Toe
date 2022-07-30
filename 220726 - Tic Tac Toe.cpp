4e26efeb46d8122f6ec023ec0afa4f228454e8df
Eza Musyarof <97140050+ezamusyarof@users.noreply.github.com>
2022-07-30 10:06:59 +0700

    Update 220726 - Tic Tac Toe.cpp

--- 220726 - Tic Tac Toe.cpp
+++ 220726 - Tic Tac Toe.cpp
@@ -7,13 +7,65 @@ string mark[9] = {" "," "," "," "," "," "," "," "," "};
 int num=0;
 int skor_kamu=0;
 int skor_aku=0;
+int game_sesion=0;
 
 void menu();
 void tampilan();
 void game();
 void pemenang();
 
+void strategi(int x, int y, int z, int cur){
+	if(mark[x]=="O"&&mark[y]=="O"&&num==cur&&mark[z]==" "){
+		table[num]=z+1;
+		mark[z]="X";
+		num++;
+	}
+}
+
+void strategi1(int x, int y, int z, int cur){
+	if(mark[x]=="X"&&mark[y]=="X"&&num==cur&&mark[z]==" "){
+		table[num]=z+1;
+		mark[z]="X";
+		num++;
+	}
+}
+
+void kamu(){
+	int user;
+	cout << "       GILIRAN KAMU '_'" << endl;
+	cout << "       Masukkan nomor => ";
+	cin >> user;
+	if(user==0||user>9){
+		tampilan(); cout << endl; kamu();
+	}else{
+		for(int k=0; k<9; k++){
+			if(table[k]==user){
+				tampilan(); cout << endl; kamu(); break;
+			}else if(k==8){
+				table[num]=user;
+				mark[user-1]="O";
+				num++;
+				tampilan();
+				pemenang();
+				break;
+			}
+		}
+	}
+}
+
+void aku(){
+	cout << "       GILIRANKU :)" << endl;
+	cout << "       Tunggu, lagi mikir ..." << endl;
+	Sleep(500);
+	if(num!=9){
+		game();
+	}
+	tampilan(); 
+	pemenang();
+}
+
 void menu(){
+	game_sesion++;
 	num = 0;
 	for(int i=0; i<9; i++){
 		table[i]=0;
@@ -23,37 +75,16 @@ void menu(){
 	for(int i=0; true; i++){
 		system("cls");
 		tampilan();
-			while(true){
-				tampilan();
-				int user;
-				cout << endl;
-				cout << "       GILIRAN KAMU '_'" << endl;
-				cout << "       Masukkan nomor => ";
-				cin >> user;
-				if(user==0||user>9){
-					cout << "Maaf nomor salah" << endl;
-				}else{
-					for(int k=0; k<9; k++){
-						if(table[k]==user){
-							cout << "Maaf nomor sudah ada" << endl;
-							break;
-						}else if(k==8){
-							table[num]=user;
-							mark[user-1]="O";
-							num++;
-							tampilan();
-							pemenang();
-							cout << "       GILIRANKU :)" << endl;
-							cout << "       Tunggu, lagi mikir ..." << endl;
-							Sleep(2000);
-							if(num!=9){game();}
-							tampilan();
-							pemenang();
-							break;
-						}
-					}
-				}
+		while(true){
+			tampilan();
+			cout << endl;
+			if(game_sesion%2==0){
+				aku(); kamu();
+			}else{
+				kamu(); aku();
 			}
+			
+		}
 	}
 }
 
@@ -103,8 +134,65 @@ void pemenang(){
 
 void game(){
 	while(true){
-		int boot;
+		int boot, cur;
 		boot = 1 + rand()%9;
+		cur = num;
+		
+		strategi1(0,1,2,cur);
+		strategi1(1,2,0,cur);
+		strategi1(2,0,1,cur);
+		strategi1(3,4,5,cur);
+		strategi1(4,5,3,cur);
+		strategi1(5,3,4,cur);
+		strategi1(6,7,8,cur);
+		strategi1(7,8,6,cur);
+		strategi1(8,6,7,cur);
+		
+		strategi1(0,3,6,cur);
+		strategi1(3,6,0,cur);
+		strategi1(6,0,3,cur);
+		strategi1(1,4,7,cur);
+		strategi1(4,7,1,cur);
+		strategi1(7,1,4,cur);
+		strategi1(2,5,8,cur);
+		strategi1(5,8,2,cur);
+		strategi1(8,2,5,cur);
+		
+		strategi1(0,4,8,cur);
+		strategi1(4,8,0,cur);
+		strategi1(8,0,4,cur);
+		strategi1(2,4,6,cur);
+		strategi1(4,6,2,cur);
+		strategi1(6,2,4,cur);
+		
+		strategi(0,1,2,cur);
+		strategi(1,2,0,cur);
+		strategi(2,0,1,cur);
+		strategi(3,4,5,cur);
+		strategi(4,5,3,cur);
+		strategi(5,3,4,cur);
+		strategi(6,7,8,cur);
+		strategi(7,8,6,cur);
+		strategi(8,6,7,cur);
+		
+		strategi(0,3,6,cur);
+		strategi(3,6,0,cur);
+		strategi(6,0,3,cur);
+		strategi(1,4,7,cur);
+		strategi(4,7,1,cur);
+		strategi(7,1,4,cur);
+		strategi(2,5,8,cur);
+		strategi(5,8,2,cur);
+		strategi(8,2,5,cur);
+		
+		strategi(0,4,8,cur);
+		strategi(4,8,0,cur);
+		strategi(8,0,4,cur);
+		strategi(2,4,6,cur);
+		strategi(4,6,2,cur);
+		strategi(6,2,4,cur);
+		
+		if(cur==num){
 		for(int k=0; k<9; k++){
 			if(table[k]==boot){
 				boot=0;
@@ -115,6 +203,7 @@ void game(){
 				num++;
 				break;
 			}
+		}
 		}
 		if(boot!=0){
 			break;
